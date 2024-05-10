76fecf6d76c3371c755d2365d8b30bdea592c55d
whai2 <whai2125@gmail.com>
2024-05-10 17:58:53 +0900

    feat : 비디오를 이미 시청한 정보에 대한 몽고 db 연동 서버 생성, 도커 컴포즈로 연동

--- history/src/index.js
+++ history/src/index.js
@@ -1,22 +1,60 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
+if (!process.env.DBHOST) {
+    throw new Error("Please specify the database host using environment variable DBHOST.");
+}
+
+if (!process.env.DBNAME) {
+    throw new Error("Please specify the name of the database using environment variable DBNAME");
+}
+
 const PORT = process.env.PORT;
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-//
 // Application entry point.
-//
 async function main() {
-
-    console.log("Hello world!");
 
     const app = express();
 
-    // ... add route handlers here ...
+    // Enables JSON body parsing for HTTP requests.
+    app.use(express.json()); 
 
+    // Connects to the database server.
+    const client = await mongodb.MongoClient.connect(DBHOST);
+
+    // Gets the database for this microservice.
+    const db  = client.db(DBNAME);
+
+    // Gets the collection for storing video viewing history.
+    const historyCollection = db.collection("history");
+
+    // Handles HTTP POST request to /viewed.
+    app.post("/viewed", async (req, res) => { // Handle the "viewed" message via HTTP POST request.
+        const videoPath = req.body.videoPath; // Read JSON body from HTTP request.
+        await historyCollection.insertOne({ videoPath: videoPath }) // Record the "view" in the database.
+
+        console.log(`Added video ${videoPath} to history.`);
+        res.sendStatus(200);
+    });
+
+    // HTTP GET route to retrieve video viewing history.
+    app.get("/history", async (req, res) => {
+        const skip = parseInt(req.query.skip);
+        const limit = parseInt(req.query.limit);
+        const history = await historyCollection.find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        res.json({ history });
+    });
+
+    // Starts the HTTP server.
     app.listen(PORT, () => {
         console.log("Microservice online.");
     });
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
 const PORT = process.env.PORT;
+
+// Send the "viewed" to the history microservice.
+function sendViewedMessage(videoPath) {
+    const postOptions = { 
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+        },
+    };
+
+    const requestBody = { 
+        videoPath: videoPath 
+    };
+
+    const req = http.request( // Send the "viewed" message to the history microservice.
+        "http://history/viewed",
+        postOptions
+    );
+
+    req.on("close", () => {
+        console.log("Sent 'viewed' message to history microservice.");
+    });
+
+    req.on("error", (err) => {
+        console.error("Failed to send 'viewed' message!");
+        console.error(err && err.stack || err);
+    });
+
+    req.write(JSON.stringify(requestBody)); // Write the body to the request.
+    req.end(); // End the request.
+}
 
 //
 // Application entry point.
@@ -15,16 +47,18 @@ async function main() {
     const app = express();
 
     app.get("/video", async (req, res) => { // Route for streaming video.
-        
+
         const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
         const stats = await fs.promises.stat(videoPath);
-    
+
         res.writeHead(200, {
             "Content-Length": stats.size,
             "Content-Type": "video/mp4",
         });
-
+    
         fs.createReadStream(videoPath).pipe(res);
+
+        sendViewedMessage(videoPath); // Sends the "viewed" message to indicate this video has been watched.
     });
 
     app.listen(PORT, () => {
@@ -37,3 +71,4 @@ main()
         console.error("Microservice failed to start.");
         console.error(err && err.stack || err);
     });
+    
